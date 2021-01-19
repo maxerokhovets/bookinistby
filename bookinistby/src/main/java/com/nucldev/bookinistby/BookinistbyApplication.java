a6b86b44a0988a75ef63beecee9cae50acb91629
maxerokhovets <maxerokhovets@gmail.com>
2021-01-19 23:22:23 +0300

    Initial commit

---
+++ bookinistby/src/main/java/com/nucldev/bookinistby/BookinistbyApplication.java
@@ -0,0 +1,13 @@
+package com.nucldev.bookinistby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookinistbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookinistbyApplication.class, args);
+	}
+
+}
